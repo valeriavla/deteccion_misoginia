ddd18511e4313526f8318d3952898918e6331669
Valeria Vera <valeriavla@hotmail.com>
2021-08-20 15:57:19 -0500

    Introduced the Tfid metric for bag of words, implemented FNN and it's cross validation

--- scripts/classifiers.py
+++ scripts/classifiers.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.style.use("ggplot")
+#plt.style.use("ggplot")
+#plt.style.use("fivethirtyeight")
+from cycler import cycler
 from itertools import chain
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix
@@ -17,93 +19,124 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import cross_val_score,KFold, StratifiedKFold,train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk import FreqDist, bigrams
+from time import perf_counter_ns
+from sklearn.datasets import make_blobs
+from mlxtend.plotting import plot_decision_regions
+from sklearn.decomposition import PCA
 
 def mnb_classify(sets,datanv): 
-    X=np.concatenate((sets[0],sets[1]))
-    y=np.concatenate((sets[2],sets[3]))    
+    #return divide_dataset(data_vect,labels,data_vect_t,labels_t,data_e,labels_e),data_t
+    #X=np.concatenate((sets[0],sets[1]))
+    #y=np.concatenate((sets[2],sets[3]))    
+    X=sets[0]
+    y=sets[1]
+    start_time=perf_counter_ns()
     mnb = MultinomialNB()
     scores = cross_val_score(mnb, X, y, scoring='accuracy', cv=StratifiedKFold(n_splits=10), n_jobs=-1)
-    print('Accuracy with cross validation: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
-    mnb.fit(sets[0],sets[2])
-    y_pred = mnb.predict(sets[1])
+    print('mnb Accuracy with cross validation: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
+    mnb.fit(sets[4],sets[5])
+    y_pred = mnb.predict(sets[2])
     y_list=y_pred.tolist()
-    with open("pred_mnb.csv", "w") as file_csv:
-        file_csv.write("predicted,label,value"+"\n")
-        for i in range (0,len(y_list)):
-            file_csv.write(str(y_list[i])+","+str(sets[3][i])+","+str(datanv[i])+"\n")
     print("MNB Accuracy:",metrics.accuracy_score(sets[3], y_pred))
     print(metrics.classification_report(sets[3], y_pred))
     matBinary = metrics.confusion_matrix(sets[3], y_pred)
     print(confusion_matrix(sets[3], y_pred))
+    end_time=perf_counter_ns()
+    with open("pred_mnb.csv", "w") as file_csv:
+        file_csv.write("predicted,label,value"+"\n")
+        for i in range (0,len(y_list)):
+            file_csv.write(str(y_list[i])+","+str(sets[3][i])+","+str(datanv[i])+"\n")
 
-def svm_classify(sets,datanv):
-    X=np.concatenate((sets[0],sets[1]))
-    y=np.concatenate((sets[2],sets[3]))  
-    model = SVC(kernel='linear')
+    print("MNB time to train: ",(end_time-start_time)/1000000000," seconds")
+
+
+def svm_classify_l(sets,datanv):
+    #sets=[data_vect,labels,data_vect_t,labels_t,data_e,labels_e]
+    X=sets[0]
+    y=sets[1]
+    start_time=perf_counter_ns()
+    model = SVC(kernel="linear")
     scores = cross_val_score(model, X, y, scoring='accuracy', cv=StratifiedKFold(n_splits=5), n_jobs=-1)
-    print('Accuracy with cross validation: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
-    expected = sets[2]
-    model.fit(sets[0], sets[2])
-    predicted = model.predict(sets[0])
+    print('svm linear Accuracy with cross validation: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
+    expected = sets[3]
+    model.fit(sets[4],sets[5])
+    predicted = model.predict(sets[2])
     y_list=predicted.tolist()
     with open("pred_svm.csv", "w") as file_csv:
         file_csv.write("predicted,label,value"+"\n")
         for i in range (0,len(y_list)):
             file_csv.write(str(y_list[i])+","+str(sets[3][i])+","+str(datanv[i])+"\n")
-    print("SVM Accuracy:",metrics.accuracy_score(sets[2], predicted))
+    print("SVM linear Accuracy:",metrics.accuracy_score(sets[3], predicted))
     print(metrics.classification_report(expected, predicted))
     print(metrics.confusion_matrix(expected, predicted))
+    end_time=perf_counter_ns()
+    print("SVM linear time to train: ",(end_time-start_time)/1000000000," seconds")
     
+    
+def svm_classify_r(sets,datanv):
+    X=sets[0]
+    y=sets[1]
+    start_time=perf_counter_ns()
+    model = SVC(kernel="rbf")
+    scores = cross_val_score(model, X, y, scoring='accuracy', cv=StratifiedKFold(n_splits=5), n_jobs=-1)
+    print('svm rbf Accuracy with cross validation: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
+    expected = sets[3]
+    model.fit(sets[4],sets[5])
+    predicted = model.predict(sets[2])
+    y_list=predicted.tolist()
+    with open("pred_svm.csv", "w") as file_csv:
+        file_csv.write("predicted,label,value"+"\n")
+        for i in range (0,len(y_list)):
+            file_csv.write(str(y_list[i])+","+str(sets[3][i])+","+str(datanv[i])+"\n")
+    print("SVM rbf Accuracy:",metrics.accuracy_score(sets[3], predicted))
+    print(metrics.classification_report(expected, predicted))
+    print(metrics.confusion_matrix(expected, predicted))
+    end_time=perf_counter_ns()
+    print("SVM rbf time to train: ",(end_time-start_time)/1000000000," seconds")
 
-def LSTM_classify(sets):
-    """
-    model.add(LSTM(4, input_shape=(1, look_back)))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
-    """
-    num_words=6967
-    max_phrase_len = 45
-    #[X_train,X_test,y_train,y_test]
-    model_lstm = Sequential()
-    model_lstm.add(Embedding(input_dim = num_words, output_dim = 256, input_length = max_phrase_len))
-    model_lstm.add(Masking(mask_value=0.0))
-    model_lstm.add(LSTM(256, dropout = 0.3, recurrent_dropout = 0.3))
-    model_lstm.add(Dense(256, activation = 'relu'))
-    model_lstm.add(Dropout(0.3))
-    model_lstm.add(Dense(5, activation = 'softmax'))
-    model_lstm.compile(
-        loss='categorical_crossentropy',
-        optimizer='Adam',
-        metrics=['accuracy']
-    )
-    history = model_lstm.fit(sets[0], sets[2], 
-                    batch_size=2048, epochs=150,
-                    validation_data=(sets[1], sets[3]))
-    model_lstm.evaluate(sets[1], sets[3])
-    """model = Sequential()
-    model.add(
-        Embedding(input_dim=num_words,
-                input_length = max_phrase_len,
-                output_dim=100,
-                weights=[embedding_matrix],
-                trainable=False,
-                mask_zero=True))
-    model.add(Masking(mask_value=0.0))
-    model.add(LSTM(64, return_sequences=False, 
-                dropout=0.1, recurrent_dropout=0.1))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_words, activation='softmax'))
-    model.compile(
-        optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    history = model.fit(X_train,  y_train, 
-                    batch_size=2048, epochs=150,
-                    callbacks=callbacks,
-                    validation_data=(X_valid, y_valid))
-    model.evaluate(X_valid, y_valid)"""
+def FFNN_classify(sets):
+    X=sets[0]
+    y=sets[1]
+    #X=sets[4]
+    #y=sets[5]
+    num_rows, num_cols = X.shape
+    num_words=num_cols
+    FNN_start_time=perf_counter_ns()
+    model = Sequential()
+    model.add(Dense(64, input_shape=(num_words,), activation="sigmoid"))
+    model.add(Dense(32, activation="sigmoid"))
+    model.add(Dense(1, activation="sigmoid"))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    history=model.fit(X, y, epochs=10, batch_size=10)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.legend(['accuracy'])
+    plt.show()
+    accuracy = model.evaluate(sets[2], sets[3])
+    print("FFNN Accuracy: ",accuracy)
+    FNN_end_time=perf_counter_ns()
+    print("FFNN time to train: ",(FNN_end_time-FNN_start_time)/1000000000," seconds")
+    return accuracy
+
+def k_cross_val(sets):
+    metrics=[]
+    accuracy=[]
+    loss=[]
+    cv=StratifiedKFold(n_splits=5)
+    X=sets[0]
+    y=sets[1]
+    for train_index, test_index in cv.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        splitted_sets=[X_train,y_train,X_test,y_test]
+        metrics.append(FFNN_classify(splitted_sets))
+    accuracy= [a[1] for a in metrics]
+    loss= [a[0] for a in metrics]
+    print(accuracy)
+    print('FFNN Accuracy with cross validation: %.3f (%.3f)' % (np.mean(accuracy), np.std(accuracy)))
+    print('loss: %.3f (%.3f)' % (np.mean(loss), np.std(loss)))
 
 def divide_dataset_random(data,labels):
     X_train, X_test, y_train, y_test = train_test_split(
@@ -111,18 +144,27 @@ def divide_dataset_random(data,labels):
            test_size = 0.25, random_state = 42)
     return[X_train,X_test,y_train,y_test,data]
 
-def divide_dataset(data,labels,data_t,labels_t):
-    X_train=data
-    X_test=data_t
-    y_train=labels
-    y_test=labels_t
-    return[X_train,X_test,y_train,y_test,data]
+def divide_dataset(data,labels,data_t,labels_t,e1,e2):
+    return[data,labels,data_t,labels_t,e1,e2]
 
 def vectorize(column):
-    vectorizer = CountVectorizer()
+    #vectorizer = CountVectorizer()
+    vectorizer=CountVectorizer(ngram_range=(1, 2))
     vectorizer.fit(column)
     vector = vectorizer.transform(column)
     return vector.toarray()
+
+def vectorize_tfidf(column):
+    start_time=perf_counter_ns()
+    vectorizer = TfidfVectorizer(ngram_range=(1, 1))
+    vector = vectorizer.fit_transform(column)
+    feature_names = vectorizer.get_feature_names()
+    dense = vector.todense()
+    denselist = dense.tolist()
+    df = pd.DataFrame(denselist, columns=feature_names)
+    end_time=perf_counter_ns()
+    print("time to transform: ",(end_time-start_time)/1000000000," seconds")
+    return df.values
 
 def transform(filename):
     filem=r'{0}.csv'.format(filename)
@@ -137,9 +179,12 @@ def transform(filename):
     data_t = list(chain(misogino_t, no_misogino_t))
     labels = np.concatenate((np.ones(len(misogino)+(len(misogino_t))),np.zeros(len(no_misogino)+len(no_misogino_t))))
     labels_t = np.concatenate((np.ones(len(misogino_t)),np.zeros(len(no_misogino_t))))
-    data_vect=vectorize(data)
+    data_vect=vectorize_tfidf(data)
     data_vect_t=np.concatenate((data_vect[850:1100],data_vect[-250:]))
-    return divide_dataset(data_vect,labels,data_vect_t,labels_t),data_t
+    data_e = np.concatenate((data_vect[0:850],data_vect[1100:1950]))
+    labels_e = np.concatenate((np.ones(len(misogino)),np.zeros(len(no_misogino))))
+    sets=[data_vect,labels,data_vect_t,labels_t,data_e,labels_e]
+    return sets,data_t
 
 def plot(matBinary):
     labels = ['negative', 'NOT negative']
@@ -174,10 +219,10 @@ def maxwords(column):
     return  max_phrase_len
 
 if __name__ == "__main__":
-    #print(metrics.SCORERS.keys())
     files=["nm_nostopwords","m_nostopwords","m_stopwords","nm_stopwords","m_NLTKstopwords","nm_NLTKstopwords"]
     sets,datanv_t=transform("preprocessed")
-    
     mnb_classify(sets,datanv_t)
-    #svm_classify(sets,datanv_t)
-    #LSTM_classify(sets)
+    svm_classify_l(sets,datanv_t)
+    svm_classify_r(sets,datanv_t)
+    FFNN_classify(sets)
+    k_cross_val(sets)
